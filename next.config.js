e71c896d95ed4285b4d0aa48a2b233d276fadc01
Guilherme Chalita <guilhermechalita@hotmail.com>
2023-12-12 19:19:55 -0300

    ci: create cypress e2e workflow

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
-    domains: ['github.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+      },
+    ],
   },
 }
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,10 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    fontFamily: {
-      sans: 'var(--font-inter)',
+    extends: {
+      fontFamily: {
+        sans: 'var(--font-inter)',
+      },
     },
   },
   plugins: [],
